bef9018b029cb713db163503274fea10da6cd26d
malang-007 <106923875+malang-007@users.noreply.github.com>
2022-06-20 11:33:47 +0500

    Create JANANO.py
    https://github.com/malang-007/JANANO.git
    cd JANANO
    python JANANO.py

---
+++ JANANO.py
@@ -0,0 +1,29 @@
+import os, platform, time
+
+try:
+
+    import requests
+
+except:
+
+    os.system('pip install requests')
+
+os.system('git pull')
+
+import requests
+
+bit = platform.architecture()[0]
+
+if bit == '64bit':
+
+    print("\n\x1b[1;92m Congratulations ! Your Device Support Tolls")
+
+    print(" \033[1;31m   Connect Vpn if Run Error!\033[1;37m")
+
+    from MALANG-007 import malang
+
+    malang()
+
+elif bit == '32bit':
+
+    print("\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools")
